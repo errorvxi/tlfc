b2f902756a081310a423e41e9caae100d36947b5
error <error.vxi@gmail.com>
2023-07-27 05:28:21 +0800

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import 'normalize.css'
+
 import './assets/css/index.less'
 
 import App from '@/App'
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
